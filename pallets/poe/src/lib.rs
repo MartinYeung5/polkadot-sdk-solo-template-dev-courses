ab8aa737a88a24ecbfeabdd2d919085cbfce5323
MartinYeung5 <luckylucky2010201@yahoo.com.hk>
2024-07-05 19:26:53 +0800

    20240705build56

--- pallets/poe/src/lib.rs
+++ pallets/poe/src/lib.rs
@@ -9,7 +9,7 @@ pub mod pallet {
 
     #[pallet::config]
     pub trait Config: frame_system::Config{
-        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_support::Config>::RuntimeEvent>;
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
         #[pallet::constant]
         type MaxClaimLength: Get<u32>;
     }
