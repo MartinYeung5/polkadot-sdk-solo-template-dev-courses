fe27fa4cd8cfe8bf360cc52d103dacffedc48874
MartinYeung5 <luckylucky2010201@yahoo.com.hk>
2024-07-05 17:46:36 +0800

    20240705build11

--- pallets/poe/src/lib.rs
+++ pallets/poe/src/lib.rs
@@ -5,6 +5,7 @@ pub mod pallet {
     use frame_support::pallet_prelude::*;
     use frame_support::pallet_prelude::*;
 
+    #[pallet::config]
     pub trait Config: frame_support::Config{
         type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_support::Config>::RuntimeEvent>;
     }
